628a27241e212b4b3d82f25f0829f1796bac5dff
Douglas Berkley <douglasmberkley@gmail.com>
2021-08-11 09:16:54 +0900

    Added restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ Restaurant.destroy_all
 puts "Creating restaurants..."
 15.times do
   Restaurant.create(
-
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    stars: rand(1..5)
   )
 end
 puts "Created #{Restaurant.count}!"
